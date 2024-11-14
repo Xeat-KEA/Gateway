9c7a4326b88dd4f8db4d4a4d3ea07c510c00f311
parkjaehak <atom8426@gmail.com>
2024-11-14 11:36:18 +0900

    modify: 클라이언트에서 Set-Cookie 접근 허용

--- src/main/java/org/codingtext/gateway/CorsConfig.java
+++ src/main/java/org/codingtext/gateway/CorsConfig.java
@@ -5,8 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsConfigurationSource;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-import org.springframework.web.reactive.config.CorsRegistry;
-import org.springframework.web.reactive.config.WebFluxConfigurer;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 
 import java.util.List;
@@ -23,9 +21,10 @@ public class CorsConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOriginPatterns(List.of("*"));
-        configuration.setAllowedMethods(List.of("*"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
         configuration.setAllowedHeaders(List.of("*"));
-        configuration.setAllowCredentials(true);
+        configuration.setExposedHeaders(List.of("Set-Cookie")); // 클라이언트에서 Set-Cookie 접근 허용
+        configuration.setAllowCredentials(true);  // 쿠키 전송 허용
         configuration.setMaxAge(3600L);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
