4380b504b86a9f8c57423d50c5a1635a0cd16e70
parkjaehak <atom8426@gmail.com>
2024-11-26 21:17:20 +0900

    add: 엑세스 토큰 만료시간에 대한 에러처리 (401 unauthorized)

--- src/main/java/org/codingtext/gateway/config/CorsConfig.java
+++ src/main/java/org/codingtext/gateway/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package org.codingtext.gateway;
+package org.codingtext.gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
